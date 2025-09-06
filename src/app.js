7319c82f49fbaf9d8c625cd6a59e7c9e628df178
sane1509 <santiagoneira52@gmail.com>
2025-09-06 17:31:43 -0300

    Control de rutas no encontradas - 404

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const { StatusCodes } = require("http-status-codes");
 
 const publicRouter = require("./routes/public.router");
 const privateRouter = require("./routes/private.router");
@@ -8,17 +9,23 @@ app.use(express.json());
 
 // Rutas públicas
 app.use(publicRouter);
-// Middleware de autenticación
-//...
-
-// Rutas privadas
+// Rutas privadas + middleware de autenticación dentro
 app.use("/v1", privateRouter);
 
-// Ruta 404
-// app.use('*', (req, res) => {
-//   res.status(StatusCodes.NOT_FOUND).json({
-//     error: 'Ruta no encontrada',
-//     path: req.originalUrl
+// 404 Not Found
+app.use((req, res) => {
+  res.status(StatusCodes.NOT_FOUND).json({
+    error: "Ruta no encontrada",
+    path: req.originalUrl,
+  });
+});
+
+// Errores 500 - Internal Server Error
+// app.use((err, req, res, next) => {
+//   console.error(err.stack);
+//   res.status(500).json({
+//     error: "Error interno del servidor",
+//     message: err.message,
 //   });
 // });
 
