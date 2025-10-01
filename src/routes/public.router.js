f32d180dd765bc73f4801809ab54acd8cf33c814
Santiago Neira <santiagoneira52@gmail.com>
2025-09-30 21:00:40 -0300

    root directory message

--- src/routes/public.router.js
+++ src/routes/public.router.js
@@ -3,8 +3,8 @@ const StatusCodes = require("http-status-codes");
 const router = express.Router();
 
 // Endpoint de prueba (ping)
-router.get("/ping", (req, res) => {
-  res.status(StatusCodes.OK).json({ message: "pong" });
+router.get("/", (req, res) => {
+  res.status(StatusCodes.OK).json({ message: "Api Cadeteria it`s works" });
 });
 
 module.exports = router;
