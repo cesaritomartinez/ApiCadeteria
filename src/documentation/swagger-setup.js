2b3f879b5f6d2431d1786a205235082d8a7184e7
César Martínez <cm330903@fi365.ort.edu.uy>
2025-10-03 14:31:02 -0300

    1

--- src/documentation/swagger-setup.js
+++ src/documentation/swagger-setup.js
@@ -1,58 +1,31 @@
-const swaggerDocument = require("./swagger.json");
-const path = require("path");
+// src/documentation/swagger-setup.js
+const swaggerUi = require('swagger-ui-express');
+const baseDoc = require('./swagger.json');
 
-const setupSwagger = (app) => {
-  console.log("setupSwagger called - registering /docs and /swagger.json routes");
+module.exports = (app) => {
+  const buildDoc = (req) => {
+    // En Vercel el protocolo real viene en x-forwarded-proto
+    const proto = (req.headers['x-forwarded-proto'] || req.protocol).split(',')[0];
+    const host = req.get('host');
 
-  // Servir el JSON de Swagger
-  app.get("/swagger.json", (req, res) => {
-    console.log("GET /swagger.json");
-    res.setHeader("Content-Type", "application/json");
-    res.send(swaggerDocument);
+    const doc = JSON.parse(JSON.stringify(baseDoc));
+    doc.servers = [{ url: `${proto}://${host}`, description: 'Current host' }];
+    return doc;
+  };
+
+  // JSON crudo (útil para probar)
+  app.get('/docs/swagger.json', (req, res) => {
+    res.json(buildDoc(req));
   });
 
-  // Servir Swagger UI con CDN (compatible con Vercel)
-  app.get("/docs", (req, res) => {
-    console.log("GET /docs");
-    const html = `
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>API Cadetería - Documentation</title>
-  <link rel="stylesheet" href="https://unpkg.com/swagger-ui-dist@5.9.0/swagger-ui.css">
-  <style>
-    body { margin: 0; padding: 0; }
-  </style>
-</head>
-<body>
-  <div id="swagger-ui"></div>
-  <script src="https://unpkg.com/swagger-ui-dist@5.9.0/swagger-ui-bundle.js"></script>
-  <script src="https://unpkg.com/swagger-ui-dist@5.9.0/swagger-ui-standalone-preset.js"></script>
-  <script>
-    window.onload = function() {
-      const ui = SwaggerUIBundle({
-        url: window.location.origin + "/swagger.json",
-        dom_id: '#swagger-ui',
-        deepLinking: true,
-        presets: [
-          SwaggerUIBundle.presets.apis,
-          SwaggerUIStandalonePreset
-        ],
-        plugins: [
-          SwaggerUIBundle.plugins.DownloadUrl
-        ],
-        layout: "StandaloneLayout",
-        persistAuthorization: true
-      });
-      window.ui = ui;
-    };
-  </script>
-</body>
-</html>`;
-    res.send(html);
+  // Swagger UI
+  app.use('/docs', (req, res, next) => {
+    const doc = buildDoc(req);
+    return swaggerUi.serve(req, res, () =>
+      swaggerUi.setup(doc, {
+        explorer: true,
+        swaggerOptions: { persistAuthorization: true },
+      })(req, res, next)
+    );
   });
 };
-
-module.exports = setupSwagger;
