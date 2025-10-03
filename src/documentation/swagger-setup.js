cc97bd70b8504e0197dd12cf43fef5caba22b2c0
Santiago Neira <santiagoneira52@gmail.com>
2025-10-03 18:52:42 -0300

    Usar Swagger UI desde CDN en lugar de npm

--- src/documentation/swagger-setup.js
+++ src/documentation/swagger-setup.js
@@ -1,7 +1,42 @@
-const swaggerUi = require("swagger-ui-express");
 const swaggerDocument = require("./swagger.json");
 
 module.exports = (app) => {
-  app.use("/docs", swaggerUi.serve);
-  app.get("/docs", swaggerUi.setup(swaggerDocument));
+  // Servir el JSON
+  app.get("/docs/swagger.json", (req, res) => {
+    res.json(swaggerDocument);
+  });
+
+  // Servir HTML estático con Swagger UI desde CDN
+  app.get("/docs", (req, res) => {
+    const html = `
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="UTF-8">
+  <title>API Cadetería - Documentación</title>
+  <link rel="stylesheet" href="https://unpkg.com/swagger-ui-dist@5.10.5/swagger-ui.css" />
+</head>
+<body>
+  <div id="swagger-ui"></div>
+  <script src="https://unpkg.com/swagger-ui-dist@5.10.5/swagger-ui-bundle.js"></script>
+  <script src="https://unpkg.com/swagger-ui-dist@5.10.5/swagger-ui-standalone-preset.js"></script>
+  <script>
+    window.onload = function() {
+      window.ui = SwaggerUIBundle({
+        url: "/docs/swagger.json",
+        dom_id: '#swagger-ui',
+        presets: [
+          SwaggerUIBundle.presets.apis,
+          SwaggerUIStandalonePreset
+        ],
+        layout: "StandaloneLayout",
+        deepLinking: true,
+        persistAuthorization: true
+      });
+    };
+  </script>
+</body>
+</html>`;
+    res.send(html);
+  });
 };
