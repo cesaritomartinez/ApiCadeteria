5bb3f5b802c455fc70bd17996d583f8912bb55f6
Santiago Neira <santiagoneira52@gmail.com>
2025-10-03 18:02:00 -0300

    /docs desde local con https

--- src/app.js
+++ src/app.js
@@ -11,6 +11,15 @@ const signupRouter = require("./routes/signup.router");
 
 const connectMongoDB = require("./repositories/mongo.client");
 
+// CORS
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  if (req.method === "OPTIONS") return res.status(200).end();
+  next();
+});
+
 app.use(express.json());
 
 // Swagger documentation (ANTES de todo para que funcione en Vercel)
--- src/documentation/swagger-setup.js
+++ src/documentation/swagger-setup.js
@@ -1,31 +1,7 @@
-// src/documentation/swagger-setup.js
-const swaggerUi = require('swagger-ui-express');
-const baseDoc = require('./swagger.json');
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("./swagger.json");
 
 module.exports = (app) => {
-  const buildDoc = (req) => {
-    // En Vercel el protocolo real viene en x-forwarded-proto
-    const proto = (req.headers['x-forwarded-proto'] || req.protocol).split(',')[0];
-    const host = req.get('host');
-
-    const doc = JSON.parse(JSON.stringify(baseDoc));
-    doc.servers = [{ url: `${proto}://${host}`, description: 'Current host' }];
-    return doc;
-  };
-
-  // JSON crudo (útil para probar)
-  app.get('/docs/swagger.json', (req, res) => {
-    res.json(buildDoc(req));
-  });
-
-  // Swagger UI
-  app.use('/docs', (req, res, next) => {
-    const doc = buildDoc(req);
-    return swaggerUi.serve(req, res, () =>
-      swaggerUi.setup(doc, {
-        explorer: true,
-        swaggerOptions: { persistAuthorization: true },
-      })(req, res, next)
-    );
-  });
+  app.use("/docs", swaggerUi.serve);
+  app.get("/docs", swaggerUi.setup(swaggerDocument));
 };
