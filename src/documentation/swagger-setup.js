61899a9d16d97a0553e6810ef57cc25191113d30
César Martínez <cm330903@fi365.ort.edu.uy>
2025-10-03 23:03:32 -0300

    estoy cansado jefe

--- src/documentation/swagger-setup.js
+++ src/documentation/swagger-setup.js
@@ -2,5 +2,29 @@ const swagger = require('swagger-ui-express');
 const swaggerJsonDoc = require('./swagger.json');
 
 module.exports = (app) => {
-  app.use("/docs", swagger.serve, swagger.setup(swaggerJsonDoc));
+  const buildDoc = (req) => {
+    // En Vercel el protocolo real viene en x-forwarded-proto
+    const proto = (req.headers['x-forwarded-proto'] || req.protocol).split(',')[0];
+    const host = req.get('host');
+
+    const doc = JSON.parse(JSON.stringify(baseDoc));
+    doc.servers = [{ url: `${proto}://${host}`, description: 'Current host' }];
+    return doc;
+  };
+
+  // JSON crudo (útil para probar)
+  app.get('/docs/swagger.json', (req, res) => {
+    res.json(buildDoc(req));
+  });
+
+  // Swagger UI
+  app.use('/docs', (req, res, next) => {
+    const doc = buildDoc(req);
+    return swaggerUi.serve(req, res, () =>
+      swaggerUi.setup(doc, {
+        explorer: true,
+        swaggerOptions: { persistAuthorization: true },
+      })(req, res, next)
+    );
+  });
 };
